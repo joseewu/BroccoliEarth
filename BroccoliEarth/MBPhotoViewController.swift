929807e3fe2006445bbc0bb825847f07af1adf06
joseewu <josee@chocolabs.com>
2018-10-16 17:43:01 +0800

    add report page and take photo page

---
+++ BroccoliEarth/MBPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MBPhotoViewController.swift
+//  BroccoliEarth
+//
+//  Created by joseewu on 2018/10/16.
+//  Copyright © 2018 com.js. All rights reserved.
+//
+
+import UIKit
+
+class MBPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BroccoliEarth/Model/MBLocation.swift
@@ -0,0 +1,17 @@
+//
+//  MBLocation.swift
+//  BroccoliEarth
+//
+//  Created by joseewu on 2018/10/16.
+//  Copyright © 2018 com.js. All rights reserved.
+//
+
+import Foundation
+
+
+struct MBLocation:Codable {
+    let name:String?
+    let latitude:Double?
+    let longitude:Double?
+    let isOverRated:Bool?
+}
